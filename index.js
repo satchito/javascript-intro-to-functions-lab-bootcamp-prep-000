2b454fd12c09dc7a06a1ed0ef8a8a371e9da7c61
Marco Sanchez <satchito@gmail.com>
2018-08-13 04:08:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,6 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase())
   return ('YES INDEED!')
   
-  if (string === "I love you, grandma.")
+  if (string === "I love you, Grandma.")
+  return ('I love you too')
 }
